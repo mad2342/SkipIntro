6e78933abd1632b8564a1a8773f06083222cf410
Mad <github@typ03.de>
2019-01-28 18:40:32 +0100

    restructure

---
+++ Source/SkipIntro/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SkipIntro")]
+[assembly: AssemblyDescription("SkipIntro Mod for Battletech by Harebrained Schemes")]
+[assembly: AssemblyProduct("SkipIntro")]
+[assembly: ComVisible(false)]
+[assembly: Guid("09D072C6-9F2E-406C-8776-664A4D7D463F")]
+[assembly: AssemblyVersion("1.3.2.*")]
+
---
+++ Source/SkipIntro/SkipIntro.cs
@@ -0,0 +1,68 @@
+﻿using System.Reflection;
+using Harmony;
+using BattleTech;
+
+
+
+namespace SkipIntro
+{
+    public class SkipIntro
+    {
+
+        public static void Init()
+        {
+            var harmony = HarmonyInstance.Create("de.mad.SkipIntro");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+
+    [HarmonyPatch(typeof(IntroCinematicLauncher), "Init")]
+    public static class IntroCinematicLauncher_Init_Patch
+    {
+        public static void Postfix()
+        {
+            Traverse.Create(typeof(IntroCinematicLauncher)).Field("state").SetValue(3);
+        }
+    }
+
+    [HarmonyPatch(typeof(SplashLauncher), "OnStart")]
+    public static class SplashLauncher_OnStart_Patch
+    {
+        public static bool Prefix(SplashLauncher __instance)
+        {
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(SplashLauncher), "OnStep")]
+    public static class SplashLauncher_OnStep_Patch
+    {
+        public static bool Prefix(SplashLauncher __instance)
+        {
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(SplashLauncher), "Start")]
+    public static class SplashLauncher_Start_Patch
+    {
+        public static bool Prefix(SplashLauncher __instance)
+        {
+            Traverse.Create(__instance).Field("currentState").SetValue(3);
+
+            var activate = Traverse.Create(__instance).Field("activate").GetValue<ActivateAfterInit>();
+            activate.enabled = true;
+
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(SplashLauncher), "Update")]
+    public static class SplashLauncher_Update_Patch
+    {
+        public static bool Prefix(SplashLauncher __instance)
+        {
+            return false;
+        }
+    }
+}
